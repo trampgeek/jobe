1a886aa3a4d06ac6a78c4b54fe6f903396e202b4
Richard Lobb <richard.lobb@canterbury.ac.nz>
2016-02-02 21:46:13 +1300

    Set default to disable keys.

--- application/config/rest.php
+++ application/config/rest.php
@@ -260,7 +260,7 @@ $config['rest_keys_table'] = 'keys';
 	) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 |
 */
-$config['rest_enable_keys'] = TRUE;
+$config['rest_enable_keys'] = FALSE;
 
 /*
 |--------------------------------------------------------------------------
