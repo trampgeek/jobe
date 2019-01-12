c0ef2983ff0cd443d425064286871a9271de6dec
rjl83 <rjl83@rjl83-eb>
2019-01-12 17:48:43 +1300

    Add code to clean file cache if volume over 95% full.

--- application/libraries/filecache.php
+++ application/libraries/filecache.php
@@ -35,6 +35,7 @@
 
 define('FILE_CACHE_BASE', '/home/jobe/files');
 define('MD5_PATTERN', '/[0-9abcdef]{32}/');
+define('MAX_PERCENT_FULL', 0.95);
 
 class FileCache {
     /**
@@ -68,20 +69,58 @@ class FileCache {
      * @param string $fileid the external file id (aka filename).
      */
     public static function file_put_contents($fileid, $contents) {
+        $freespace = disk_free_space(FILE_CACHE_BASE);
+        $volumesize = disk_total_space(FILE_CACHE_BASE);
+        if ($freespace / $volumesize > MAX_PERCENT_FULL) {
+            self:: clear_cache();
+        }
         if (preg_match(MD5_PATTERN, $fileid) !== 1) {
-            file_put_contents(FILE_CACHE_BASE . '/' . $fileid, $contents);
+            $result = @file_put_contents(FILE_CACHE_BASE . '/' . $fileid, $contents);
         } else {
             $topdir = FILE_CACHE_BASE . '/' . substr($fileid, 0, 2);
             $seconddir = $topdir . '/' . substr($fileid, 2, 2);
             $fullpath = $seconddir . '/' . substr($fileid, 4);
             if (!is_dir($topdir)) {
-                mkdir($topdir, 0751);
+                @mkdir($topdir, 0751);
             }
             if (!is_dir($seconddir)) {
-                mkdir($seconddir, 0751);
+                @mkdir($seconddir, 0751);
             }
-            file_put_contents($fullpath, $contents);
+            $result = @file_put_contents($fullpath, $contents);
         }
+        return $result;
+    }
+
+    /**
+     * Delete all files in the file cache that were last accessed over two
+     * days ago.
+
+     * This method is called by the file_put_contents method
+     * if, before adding a file, the volume containing the file cache directory
+     * (/home/jobe/files) is over 95% full.
+     *
+     * If Jobe is providing services just to a Moodle/CodeRunner instance, this
+     * method should rarely if ever be called. Usually (and indeed always, prior
+     * to 2019) the only files being uploaded are the support files attached to
+     * questions by question authors. The total of all such files is usually small
+     * enough to leave plenty of space in a typical Linux install.
+
+     * However, if question authors are allowing/requiring students to attach
+     * files to their submissions, the file cache could fill up over time and/or
+     * with large classes and/or with large attachments. Then a cache clean up
+     * will be required. However, it should still be a very rare event and is
+     * certainly no worse than switching CodeRunner to a new Jobe server.
+     *
+     * Note that CodeRunner always tries running a job without first uploading
+     * files. Only if the run fails with a 404 Not Found do the files then
+     * get uploaded. With this mode of operation, deleting files unused for
+     * 2 days should be safe. However, non-CodeRunner users using HEAD to
+     * check file existence need to be prepared to re-upload files in the event
+     * of a 404 Not Found.
+     */
+    public static function clean_cache() {
+        log_message('info', '*jobe* ', 'Cleaning file cache');
+        shell_exec("find " . FILE_CACHE_BASE . " -type f -atime +1 -delete &");
     }
 
 
